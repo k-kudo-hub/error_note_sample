d0772cbb11f4c55b6b1220fc156c073518da39e5
k-kudo-hub <tongariisaibou@gmail.com>
2021-03-25 23:02:28 +0900

    Revert "Revert "バリデーションの設定""
    This reverts commit f147a0ee24ee548767993c13a22d36e4b38d732a.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :kana, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :kana, :email])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :logs
+  
+  with_options presence: { message: 'が入力されていません。'} do 
+    validates :name
+    validates :kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナのみ登録できます。' }
+    validates :email
+  end
 end
