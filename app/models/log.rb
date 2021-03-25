f147a0ee24ee548767993c13a22d36e4b38d732a
k-kudo-hub <tongariisaibou@gmail.com>
2021-03-25 22:47:30 +0900

    Revert "バリデーションの設定"
    This reverts commit ae834c8910c3e5cb96cbd67ed87e7bceb18d7045.

--- app/models/log.rb
+++
@@ -1,8 +0,0 @@
-class Log < ApplicationRecord
-  belongs_to :user
-  with_options presence: { message: 'が入力されていません。'} do 
-    validates :title
-    validates :error
-    validates :release
-  end
-end
