359869b7432688fe1df4251a219848ac29a5d215
toshiro-koba <kobato.231.0625@gmail.com>
2021-04-21 01:32:15 +0900

    プログラミング言語を別テーブルに分割

---
+++ db/migrate/20210420150106_create_log_languages.rb
@@ -0,0 +1,9 @@
+class CreateLogLanguages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :log_languages do |t|
+      t.references :log, foreign_key: true
+      t.references :language, foreign_key: true
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210420153943_remove_language_to_logs.rb
@@ -0,0 +1,9 @@
+class RemoveLanguageToLogs < ActiveRecord::Migration[6.1]
+  def up
+    remove_column :logs, :language, :integer
+  end
+
+  def down
+    add_column :logs, :language, :integer
+  end
+end
