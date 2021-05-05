4afed4156fc6f0a81da500c7b2bad9d491c040cd
toshiro-koba <kobato.231.0625@gmail.com>
2021-05-05 17:13:39 +0900

    言語一覧を定義する際、言語に関連するレコードも削除するようdestroy_allに変更

--- db/migrate/20210504125630_define_languages.rb
+++ db/migrate/20210504125630_define_languages.rb
@@ -1,6 +1,6 @@
 class DefineLanguages < ActiveRecord::Migration[6.1]
   def change
-    Language.delete_all
+    Language.destroy_all
     ActiveRecord::Base.connection.execute('alter table languages auto_increment = 1;')
 
     languages = ["C", "C#", "C++", "COBOL", "Go", "Java", "JavaScript", "Kotlin", "Kuin", "LISP", "MATLAB", "Node.js", "PHP", "Perl", "Python", "R", "Ruby", "Rust", "Scala", "Swift", "TypeScript", "VisualBasic.NET"]
