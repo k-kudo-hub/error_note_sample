10efe7c269927aafe80743605ad90538737da4de
k-kudo-hub <tongariisaibou@gmail.com>
2021-05-22 12:05:46 +0900

    turbolinks読み込み時にランキングを描画するよう設定した

---
+++ app/javascript/packs/user_rank.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import User from '../userRank.vue'
+
+document.addEventListener('turbolinks:load', () => {
+  const insertTarget = document.getElementById('nav_bar_lang_rank')
+  const alreadyInsert = document.getElementById('rank_content')
+  if(alreadyInsert) {
+    while(alreadyInsert.lastChild){
+      alreadyInsert.removeChild(alreadyInsert.lastChild);
+    }
+  }
+  if(insertTarget) {
+    const usr = new Vue({
+      render: h => h(User)
+    }).$mount()
+    insertTarget.insertAdjacentElement('afterbegin', usr.$el)
+    console.log(usr)
+  }
+})
+
