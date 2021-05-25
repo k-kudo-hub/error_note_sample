9e7437d2ec5335516e28edc69d0ea2d1480e9bff
k-kudo-hub <tongariisaibou@gmail.com>
2021-05-25 22:04:58 +0900

    処理をまとめ、データを整形した

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -3,30 +3,22 @@ class Api::V1::LogsController < ApplicationController
     render json: { error: '404 not found' }, status: 404
   end
 
-  def languages
-    top_lang = Language.rank
-    render json: top_lang
-  end
-
-  def count_languages
-    logs = LogLanguage.group(:language_id).order('count(language_id) desc').limit(5).count
+  def lang_count
+    langs = Language.rank
+    counts = LogLanguage.group(:language_id).order('count(language_id) desc').limit(5).count
     array = []
-    logs.each do |log|
-      array << log[1]
+    langs.zip(counts) do |lang, count|
+      array.push(name: lang.name, count: count[1])
     end
     render json: array
   end
 
-  def stocks
-    top_log = Log.rank
-    render json: top_log
-  end
-
-  def count_stocks
-    logs = Stock.group(:log_id).order('count(log_id) desc').limit(5).count
+  def stock_count
+    logs = Log.rank
+    counts = Stock.group(:log_id).order('count(log_id) desc').limit(5).count
     array = []
-    logs.each do |log|
-      array << log[1]
+    logs.zip(counts) do |log, count|
+      array.push(id: log.id, title: log.title, count: count[1], user_id: log.user_id)
     end
     render json: array
   end
