950c30d82da39a5a9d28a38eac32d343f7652433
k-kudo-hub <tongariisaibou@gmail.com>
2021-05-29 11:04:21 +0900

    ログインしていない場合は記録ボタンを描画しないよう設定した

--- app/javascript/packs/logs_create.js
+++ app/javascript/packs/logs_create.js
@@ -3,7 +3,11 @@ import User from '../logsCreate.vue'
 
 document.addEventListener('turbolinks:load', () => {
   const insertTarget = document.getElementById('body')
+  const doNotInsert = document.getElementById('sign_in_btn')
   const alreadyInsert = document.getElementById('log_modal')
+  if(doNotInsert) {
+    return false;
+  }
   if(alreadyInsert) {
     while(alreadyInsert.lastChild){
       alreadyInsert.removeChild(alreadyInsert.lastChild);
