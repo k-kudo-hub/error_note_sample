31f8f6054bcf174fb68123ef7b0d5fd2d1c18d4c
k-kudo-hub <tongariisaibou@gmail.com>
2021-05-30 12:55:52 +0900

    App.vueの描画を定義

---
+++ app/javascript/packs/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from '../App.vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
+
+document.addEventListener('turbolinks:load', () => {
+  // const exclusionPath = "users$"
+  const insertTarget = document.getElementById('body')
+  const alreadyInsert = document.getElementById('app')
+  if(alreadyInsert) {
+    while(alreadyInsert.lastChild){
+      alreadyInsert.removeChild(alreadyInsert.lastChild);
+    }
+  }
+  if(insertTarget) {
+    const usr = new Vue({
+      vuetify: new Vuetify(),
+      render: h => h(App)
+    }).$mount()
+    insertTarget.insertAdjacentElement('afterbegin', usr.$el)
+  }
+})
