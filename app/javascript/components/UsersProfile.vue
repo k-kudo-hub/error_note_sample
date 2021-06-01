1363c10761c135cb46c9a03044ba7c5748097909
k-kudo-hub <tongariisaibou@gmail.com>
2021-06-01 20:46:29 +0900

    ユーザープロフィール部分を別コンポーネントに切り分けた

---
+++ app/javascript/components/UsersProfile.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="app-bottom-inner">
+    <div class="bottom-profile__upper">
+      <div class="profile-img__container">
+        <img v-if="user.picture != null" :src="user.picture">
+        <img v-else src="../../assets/images/human.png">
+      </div>
+      <div class="profile-introduce__container">
+        <div class="profile-introduce__name">
+          <h2>{{ user.name }}</h2>
+        </div>
+        <div class="profile-introduce__word">
+          {{ user.introduce }}
+        </div>
+      </div>
+    </div>
+    <div class="bottom-profile__lower">
+      <div class="profile-btn__container">
+        <template v-if="use_case == 'log'">
+          <a v-if="(currentUser.auth == true) && (currentUser.id != user.id)" class="btn-filled">フォローする</a>
+          <a class="btn-default">プロフィールへ</a>
+        </template>
+        <template v-else>
+          <template v-if="(currentUser.auth == true) && (currentUser.id == user.id)">
+            <a class="btn-filled">プロフィール編集</a>
+            <a class="btn-default">ログアウト</a>
+          </template>
+          <template v-else-if="(currentUser.auth == true) && (currentUser.id != user.id)">
+            <a class="btn-filled">フォローする</a>
+          </template>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      
+    }
+  },
+  props: {
+    user: {
+      id: 0,
+      name: "",
+      picture: "",
+      introduce: "",
+    },
+    currentUser: {
+      id: null,
+      picture: null,
+      auth: false,
+    },
+    use_case: "",
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
