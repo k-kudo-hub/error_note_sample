d5f45702c3e65e1d9b7a70ed29e483d559ba71c2
k-kudo-hub <tongariisaibou@gmail.com>
2021-06-30 08:16:28 +0900

    本番環境の設定を一時的に修正した

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
 app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
-working_directory "#{app_path}/current"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+working_directory "#{app_path}"
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 timeout 15
 
 preload_app true
