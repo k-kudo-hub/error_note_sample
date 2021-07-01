e185a06b5f3f2483d992c1e81ff523ca70e15ea1
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-01 17:27:02 +0900

    設定の微調整

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
 working_directory "#{app_path}"
 pid "#{app_path}/tmp/pids/unicorn.pid"
