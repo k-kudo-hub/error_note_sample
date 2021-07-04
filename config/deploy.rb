641766dbcaad72b60e6081cb104a892d8aa3b851
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-04 21:06:03 +0900

    デプロイ時にブランチを指定できるようにした

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ lock "~> 3.16.0"
 
 set :application, "error_note_sample"
 set :repo_url, "git@github.com:k-kudo-hub/error_note_sample.git"
+set :branch, ENV['BRANCH'] || "master"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('node_modules', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public')
 set :linked_files, fetch(:linked_files, []).push('config/master.key', '.env')
