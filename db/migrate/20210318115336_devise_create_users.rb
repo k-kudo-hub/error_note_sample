8d897a99670e89b18391f30b1d46fa98ea9d5cd0
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-15 07:25:12 +0900

    不要な改行の削除

--- db/migrate/20210318115336_devise_create_users.rb
+++ db/migrate/20210318115336_devise_create_users.rb
@@ -5,12 +5,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
-
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-
       t.datetime :remember_created_at
-
       t.timestamps null: false
     end
 
