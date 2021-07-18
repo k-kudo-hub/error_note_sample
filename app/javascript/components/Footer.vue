3f61c2b2ae55470d8fa448b80164fa129804d284
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-18 10:56:58 +0900

    SNSリンクの調整

--- app/javascript/components/Footer.vue
+++ app/javascript/components/Footer.vue
@@ -5,10 +5,10 @@
         <a href="/" class="footer-site-name">Error Note</a>
         <p>Error will give you a hint.</p>
         <div class="footer-sns-area">
-          <a href="https://ja-jp.facebook.com/" target="_blank" rel="noopener npreferrer">
-            <img src='../../assets/images/facebook.png' height="20px" width="20px">
+          <a :href="laprasURL" target="_blank" rel="noopener npreferrer">
+            <img src='../../assets/images/brain.png' height="20px" width="20px">
           </a>
-          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+          <a :href="twitterURL" target="_blank" rel="noopener noreferrer">
             <img src='../../assets/images/twitter.png' height="20px" width="20px">
           </a>
         </div>
@@ -17,14 +17,11 @@
         <div class="footer-right-start">
           <p>Error Note</p>
           <a href="#">利用規約</a>
-          <a href="#">サイトマップ</a>
           <a href="#">お問い合わせ</a>
         </div>
         <div class="footer-right-end">
           <p>Joint Dev inc.</p>
-          <a href="#">会社概要</a>
-          <a href="#">採用情報</a>
-          <a href="#">チームブログ</a>
+          <a :href="githubURL" target="_blank">Github<i class="fas fa-external-link-alt ml-5"></i></a>
         </div>
       </div>
     </div>
@@ -35,7 +32,13 @@
 </template>
 <script>
 export default {
-  
+  data(){
+    return {
+      laprasURL: "https://lapras.com/public/GCUQE8M",
+      twitterURL: "https://twitter.com/WAKO_program",
+      githubURL: "https://github.com/k-kudo-hub",
+    }
+  }
 }
 </script>
 <style scoped>
