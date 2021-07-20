abb49ae3b6ca4a1be6225d1f60403e0189fd5faf
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-20 14:09:28 +0900

    変更の可能性がある値をdataにまとめた

--- app/javascript/components/shared/Footer.vue
+++ app/javascript/components/shared/Footer.vue
@@ -2,14 +2,14 @@
   <div class="footer-area">
     <div class="footer-top">
       <div class="footer-left">
-        <a href="/" class="footer-site-name">Error Note</a>
-        <p>Error will give you a hint.</p>
+        <a href="/" class="footer-site-name">{{ site.name }}</a>
+        <p>{{ site.subtitle }}</p>
         <div class="footer-sns-area">
-          <a :href="laprasURL" target="_blank" rel="noopener npreferrer">
-            <img src='../../assets/images/brain.png' height="20px" width="20px">
+          <a :href="url.lapras" target="_blank" rel="noopener npreferrer">
+            <img :src="image.lapras" height="20px" width="20px">
           </a>
-          <a :href="twitterURL" target="_blank" rel="noopener noreferrer">
-            <img src='../../assets/images/twitter.png' height="20px" width="20px">
+          <a :href="url.twitter" target="_blank" rel="noopener noreferrer">
+            <img :src="image.twitter" height="20px" width="20px">
           </a>
         </div>
       </div>
@@ -22,22 +22,35 @@
         </div>
         <div class="footer-right-end">
           <p>Joint Dev inc.</p>
-          <a :href="githubURL" target="_blank" rel="noopener noreferrer">Github<i class="fas fa-external-link-alt ml-5"></i></a>
+          <a :href="url.github" target="_blank" rel="noopener noreferrer">Github<i class="fas fa-external-link-alt ml-5"></i></a>
         </div>
       </div>
     </div>
     <div class="footer-bottom">
-      <p class="footer-copy">©️ 2021 Joint Dev Inc.</p>
+      <p class="footer-copy">{{ site.copy }}</p>
     </div>
   </div>
 </template>
 <script>
+import LaprasImage from "../../../assets/images/brain.png"
+import TwitterImage from "../../../assets/images/twitter.png"
 export default {
   data(){
     return {
-      laprasURL: "https://lapras.com/public/GCUQE8M",
-      twitterURL: "https://twitter.com/WAKO_program",
-      githubURL: "https://github.com/k-kudo-hub",
+      image: {
+        lapras: LaprasImage,
+        twitter: TwitterImage,
+      },
+      url: {
+        lapras: "https://lapras.com/public/GCUQE8M",
+        twitter: "https://twitter.com/WAKO_program",
+        github: "https://github.com/k-kudo-hub",
+      },
+      site: {
+        name: "Error Note",
+        subtitle: "Error will give you a hint.",
+        copy: "©️ 2021 Joint Dev Inc."
+      }
     }
   }
 }
