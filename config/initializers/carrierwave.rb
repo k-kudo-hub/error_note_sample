b8c45b870fecb99f9bc82b876b35dfe5699e12aa
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-23 11:47:43 +0900

    エラー対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,7 @@ unless Rails.env.development? || Rails.env.test?
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
       path_style: true
     }
   end
