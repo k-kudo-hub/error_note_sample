85a29c7dbcdda294d33b008f051b844d3e3dd8fd
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-23 11:45:43 +0900

    エラー対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
-    config.cache_storage :fog
+    config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'error-note'
     config.fog_credentials = {
@@ -12,6 +12,7 @@ unless Rails.env.development? || Rails.env.test?
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
+      path_style: true
     }
   end
 end
