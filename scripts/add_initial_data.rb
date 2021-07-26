5c6fd9532626c8b3c76255f92888b77016dc133b
k-kudo-hub <tongariisaibou@gmail.com>
2021-07-26 17:32:30 +0900

    WIP:ダミーデータ作成

--- scripts/add_initial_data.rb
+++ scripts/add_initial_data.rb
@@ -1,4 +1,4 @@
-# C lang
+# C Lang
 c_1 = Log.create(
   user_id: 1,
   title: "No such file or directory",
@@ -32,7 +32,7 @@ c_2 = Log.create(
   release: true,
 )
 
-# C# lang
+# C# Lang
 error = <<~EOS
   Index was out of range.
   インデックスは配列の範囲外ですと怒られた。該当のコードは以下の通り。
@@ -51,4 +51,133 @@ cs_1 = Log.create(
   user_id: 1,
   release: true,
 )
+# error = <<~EOS
+#   
+# EOS
+# solution = <<~EOS
+#   
+# EOS
+# cs_1 = Log.create(
+#   error: error,
+#   languages: Language.where(name: "C#"),
+#   solution: solution,
+#   title: '',
+#   user_id: 1,
+#   release: true,
+# )
+
+# C++ Lang
+error = <<~EOS
+  C2513.cpp エラー。該当コードは以下。
+  int main(){
+    int = 1;
+  }
+EOS
+solution = <<~EOS
+  変数名を書き忘れていた。
+  int main(){
+    int i = 1;
+  }
+  としたところ、エラーは解消した。
+EOS
+cp_1 = Log.create(
+  error: error,
+  languages: Language.where(name: "C++"),
+  solution: solution,
+  title: 'コンパイルエラー C2513',
+  user_id: 1,
+  release: true,
+)
+error = <<~EOS
+  C2569.cpp エラー。コードは以下。
+  union ubase{};
+  class cHasPubBase : public ubase {};
+EOS
+solution = <<~EOS
+  共用体は基底クラスとして使用できないとのこと。
+  struct sbase{};
+  class cHasPubBase : public sbase {};
+  ↑のように構造体に変更する必要がある。
+EOS
+cp_1 = Log.create(
+  error: error,
+  languages: Language.where(name: "C++"),
+  solution: solution,
+  title: 'コンパイルエラー C2569 EnumOrUnion...',
+  user_id: 1,
+  release: true,
+)
+
+# COBOL
+error = <<~EOS
+  コンパイルエラー。
+  変数が定義されていないとのこと。
+  DATA DIVISION
+    01 SUBSTRCT PIC 9(5).
+  PROCEDURE DIVISION
+    MOVE 1 TO SUBSTRACT.
+EOS
+solution = <<~EOS
+  スペルミスが原因だった。
+  DATA DIVISIONでSUBSTRCTとしていたところをSUBSTRACTとしたところ、エラーは解消した。
+EOS
+cobol_1 = Log.create(
+  error: error,
+  languages: Language.where(name: "COBOL"),
+  solution: solution,
+  title: 'スペルミスでコンパイルエラー',
+  user_id: 1,
+  release: true,
+)
+error = <<~EOS
+  コンパイルエラー。ネストしたから？
+  IF SUUJI1 > 2
+  THEN
+    IF SUUJI1 > 5
+    THEN
+      SUBTRACT 5 FROM SUUJI1
+    ELSE
+      ADD 1 TO SUUJI1
+  END-IF.
+EOS
+solution = <<~EOS
+  普通にケアレスミス。END IFを入れるのを忘れていた。
+EOS
+cp_1 = Log.create(
+  error: error,
+  languages: Language.where(name: "COBOL"),
+  solution: solution,
+  title: 'ENDIFを忘れてコンパイルエラー',
+  user_id: 1,
+  release: true,
+)
+
+# Go Lang
+error = <<~EOS
+  sample.go:6:syntax error: unexpected semicolon or newline before {
+  package sample
+
+  import "fmt"
+  func sample()
+  {
+    fmt.Println("hello world!")
+  }
+EOS
+solution = <<~EOS
+  関数の開始中括弧を改行していたことが原因らしい。
+  func sample(){
+    fmt.Println("hello world!")
+  }
+  このようにしたら直った。
+  構文チェックが厳しい笑
+EOS
+go_1 = Log.create(
+  error: error,
+  languages: Language.where(name: "Go"),
+  solution: solution,
+  title: 'unexpected samicolon or newline before',
+  user_id: 1,
+  release: true,
+)
+
 
