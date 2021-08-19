177f561d7c5651ff3ace2f3fcc831addf93a73c8
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-19 21:22:49 +0900

    usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_062901) do
+ActiveRecord::Schema.define(version: 2021_08_18_231518) do
 
   create_table "languages", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2021_07_18_062901) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.boolean "accepted", default: false, null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
