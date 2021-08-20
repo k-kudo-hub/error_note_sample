8ed0cff06e849fce749ee27f5e1b2b24960d9891
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-20 19:26:02 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   root 'logs#index'
   resources :users, only: %i[show] do
