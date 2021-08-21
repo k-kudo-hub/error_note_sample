69a6df02d4b4af0746d2e23c8f2c4e6a22198078
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-21 16:13:58 +0900

    スクリプトファイルの修正

--- scripts/notification_202108210858.rb
+++ scripts/notification_202108210858.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+Notification.destroy_all
+ActiveRecord::Base.connection.execute('alter table notifications auto_increment = 1;')
 content = <<~EOS
   ErrorNoteをご利用いただきありがとうございます。
   この度、Twitterアカウントでログインができるようになりました。
   「ログイン」より、「Twitterアカウントでログイン」ボタンを押していただくと、Twitterアカウントでログインいただけます。
 EOS
 Notification.create!(
-  title: 'Twitterアカウントでログインできるようになりました。',
+  title: '【お知らせ】Twitterアカウントでログインできるようになりました。',
   content: content,
   is_important: false,
   below_header: true
