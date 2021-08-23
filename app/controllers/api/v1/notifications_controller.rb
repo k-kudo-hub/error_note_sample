6db181638983e39492c693b6e7608bbbd8724bec
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-23 22:59:18 +0900

    リファクタリング

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::NotificationsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound do |_exception|
     render json: { error: '404 not found' }, status: :not_found
